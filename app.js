933f45593dbc6140190219bdf37a9932335afd22
Matias Agüero <agueromatias214@gmail.com>
2025-05-12 09:54:36 -0300

    Se agregó alerta de opción incorrecta

--- app.js
+++ app.js
@@ -6,18 +6,24 @@ let history = []
 function convertCurrency(pesos){
  return pesos/precioUSD;
 }
+
 //Creación del bucle con opciones de cliente.
 do {
 opciones =parseFloat(prompt ('¿Desea realizar una conversion? \n 1)Si \n 2)No'));
 switch (opciones){
    case 1: 
-    pesos = prompt('Ingrese cantidad en pesos');
+    pesos = prompt('Ingrese cantidad en pesos'); //Bloque de ingreso de datos.
     alert('El monto ingresado es de '+ convertCurrency(pesos).toFixed(2)+ 'USD');
     history.push({
     tipo : 'divisa',
     entrada : pesos,
     salida: convertCurrency(pesos).toFixed(2)+'USD',
-    fecha :new Date().toLocaleString()}) //Imprime la fecha en el array de salida. 
+    fecha :new Date().toLocaleString()}) //Imprime la fecha en el array de salida.
+     
+}
+//Evalúa si las opciones ingresadas son correctas, sino alerta de un error
+if (opciones > 2 && opciones <1){ 
+    alert ('El valor que ingresó es inválido, porfavor seleccione 1 o 2');
 }
 }
 while(opciones!=2);
