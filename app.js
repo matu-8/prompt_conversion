83a111e76e7ed0ab898683ebf96454cb690479df
Matias Agüero <agueromatias214@gmail.com>
2025-05-12 08:25:01 -0300

    Creación de opciones con bucles

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-let opcion = prompt("Ingrese valor en pesos");
-let divisa1 = "ARG";
-let USD = 1140;
+let opciones,menu, pesos;
+const precioUSD = 1300;
+let history = []
+//     tipo : divisa,
+//     entrada = '1300 ARG',
+//     salida= '1 USD',
+//     fecha = new date().ToLocaleString()
+// function convertCurrency (pesos){
+//     return precioUSD*pesos;
+// }
+function convertCurrency(pesos){
+ return pesos/precioUSD;
+}
+do {
+opciones = prompt ('¿Desea realizar una conversion? \n 1)Si \n 2)No');
+switch (opciones){
+   case '1': 
+    pesos = prompt('Ingrese cantidad en pesos');
+    alert('El momnto ingresado es de '+ convertCurrency(pesos).toFixed(2)+ 'USD');
+}
+}
+while(opciones!='2');
